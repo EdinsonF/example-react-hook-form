9b7498db06d5801c4ac75a283498cf7228b4cafc
edinson_figueroa <figueroaedinson@gmail.com>
2021-08-08 22:33:50 -0500

    finished example

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
-import Header from './Header';
+
+let counter = 0;
 
 const Form = () => {
 
+  counter++;
+
   const [form, setForm] = useState({});
   const [result, setResult] = useState("");
-
-  
 
   const handleInput = e => {
     setForm({
@@ -17,23 +18,31 @@ const Form = () => {
   const sendForm = (e) => {
     e.preventDefault();
     setResult(JSON.stringify(form));
-  
   }
 
   return ( 
     <>
-    {console.log("hola")}
-      <Header/>
+    <div className="container">
+        <div className="header">
+          <h2>Form HandleChange</h2>
+          <span className="counter">Render {counter}</span>
+        </div>
+        
+        <form onSubmit={sendForm} className="formulario">
+            <div className="campo">
+              <label>Name:</label>
+              <input type="text" name="nombre"  onChange={handleInput}/>
+            </div>
 
-      <h2>Form HandleChange</h2>
-      <form onSubmit={sendForm}>
-        <label>Name:</label>
-        <input type="text" name="nombre"  onChange={handleInput}/>
-        <label>Last Name:</label>
-        <input type="text" name="apellido"  onChange={handleInput}/>
-        {result}
-        <input type="submit" value="Send" />
-      </form>
+            <div className="campo">
+              <label>Last Name:</label>
+              <input type="text" name="apellido"  onChange={handleInput}/>
+              {result}
+            </div>
+
+          <input type="submit" value="Send" />
+        </form>
+     </div> 
     </>
    );
 }
--- src/Components/FormHook.js
+++ src/Components/FormHook.js
@@ -1,25 +1,81 @@
 import React, {useState} from 'react';
-import Header from './Header';
+import { useForm } from "react-hook-form";
 
-
+let counter= 0;
 const FormHook = () => {
 
-  const [form, setForm] = useState({});
+  counter++;
 
-  
+  const [result, setResult] = useState();
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  console.log(errors?.nombre?.type);
+  const onSubmit = (data) => {
+   console.log("entro");
+    setResult(JSON.stringify(data))
+  }
+
 
   return ( 
     <>
-    
+    <div className="container">
+      <div className="header">
+        <h2>Form Hook</h2>
+        <span className="counter">Render {counter}</span>
+      </div>
+      
+        <form onSubmit={handleSubmit(onSubmit)} className="formulario"> 
+          
+          <div className="campo">
+            <label>Name:</label>
+            <input type="text"
+              {...register("nombre", 
+                  { 
+                    required: true, 
+                    maxLength: 20, 
+                    minLength: 4 }
+              )}
+            />
+            <div className="mensajes">
+              {errors?.nombre?.type === "required" 
+                  && <p>This field is required</p>}
+              {errors?.nombre?.type === "maxLength" 
+                  && 
+                <p> name cannot exceed 20 characters</p>
+              }
+              {errors?.nombre?.type === "minLength" 
+                  && 
+                <p> name must have min 4 characters</p>
+              }
+            </div>
+          </div>
+            <div className="campo">
+            <label>Last Name:</label>
+            <input type="text"
+              {...register("apellido", 
+                { 
+                  required: true, 
+                  maxLength: 20, 
+                  minLength: 4 }
+              )}
+            />
+          {errors?.apellido?.type === "required" 
+              && <p>This field is required</p>}
+          {errors?.apellido?.type === "maxLength" 
+              && 
+            <p> name cannot exceed 20 characters</p>
+          }
+          {errors?.apellido?.type === "minLength" 
+              && 
+            <p> name must have min 4 characters</p>
+          }
+          {result}
+            </div>
+          <input type="submit" value="Send" />
 
-      <h2>Form Hook</h2>
-      <form>
-        <label>Name:</label>
-        <input type="text" name="nombre" />
-        <label>Last Name:</label>
-        <input type="text" name="apellido" />
-        <input type="submit" value="Send" />
-      </form>
+        
+        </form>
+      </div>
     </>
    );
 }
